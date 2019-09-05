3113af1b1dfc7aeb20a0bd273fe035a4dc597ff5
Filip Škrlj <49915188+FikusHladni@users.noreply.github.com>
2019-09-05 20:31:11 +0200

    Update vmesnik.py

--- vmesnik.py
+++ vmesnik.py
@@ -8,46 +8,17 @@ Naslov = stiri_v_vrsto.Igra()
 def tece() :
     return bottle.template("template.tpl", Igra = Igra)
 
-@bottle.post("/0/")
-def vstavljanje1():
-    Igra.potek(0)
-    return bottle.template("template.tpl", Igra = Igra)
-
-@bottle.post("/1/")
-def vstavljanje2():
-    Igra.potek(1)
-    return bottle.template("template.tpl", Igra = Igra)
-
-@bottle.post("/2/")
-def vstavljanje3():
-    Igra.potek(2)
-    return bottle.template("template.tpl", Igra = Igra)
-
-@bottle.post("/3/")
-def vstavljanje4():
-    Igra.potek(3)
-    return bottle.template("template.tpl", Igra = Igra)
-
-@bottle.post("/4/")
-def vstavljanje5():
-    Igra.potek(4)
-    return bottle.template("template.tpl", Igra = Igra)
-
-@bottle.post("/5/")
-def vstavljanje6():
-    Igra.potek(5)
-    return bottle.template("template.tpl", Igra = Igra)
-
-@bottle.post("/6/")
-def vstavljanje7():
-    Igra.potek(6)
-    return bottle.template("template.tpl", Igra = Igra)
+@bottle.post("/<stolpec:int>/")
+def vstavljanje(stolpec):
+    Igra.potek(stolpec)
+    bottle.redirect("/")
 
 
-@bottle.post("/7/")
+@bottle.post("/nova/")
 def nova():
     Igra.nova_igra()
-    return bottle.template("template.tpl" , Igra = Igra)
+    bottle.redirect("/")
+
          
     
 bottle.run(reloader=True , debug=True)
